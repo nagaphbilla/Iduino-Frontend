7f157e9b1ad1395575e28850438f28c57bba7f7b
vedavyas <vedavyas14042003@gmail.com>
2023-04-18 21:20:31 +0530

    made responsive

--- src/components/Slide2.jsx
+++ src/components/Slide2.jsx
@@ -14,7 +14,7 @@ export default function Slide2() {
         </p>
       </div>
       <div className="flex-direction py-10 md:flex gap-10 px-5 md:w-2/3">
-        <div className="flex flex-col gap-2 button-clr-primary px-4 py-4 rounded-xl justify-center min-h-fit">
+        <div className="flex flex-col gap-2 button-clr-primary px-4 py-7 rounded-xl justify-center min-h-fit">
           <span className="w-9 h-9 rounded-full bg-white background_img_card1" />
           <h4 className="text-white font-bold">Professional Teacher</h4>
           <p className="text-white font-thin text-sm">
@@ -24,7 +24,7 @@ export default function Slide2() {
             iusto, illum omnis sapiente accusamus?
           </p>
         </div>
-        <div className="flex flex-col gap-2 button-clr-primary px-4 py-4 rounded-xl justify-center min-h-fit">
+        <div className="flex flex-col gap-2 button-clr-primary px-4 py-7 rounded-xl justify-center min-h-fit">
           <span className="w-9 h-9 rounded-full bg-white background_img_card2" />
           <h4 className="text-white font-bold">Course Certificate </h4>
           <p className="text-white font-thin text-sm">
@@ -34,7 +34,7 @@ export default function Slide2() {
             iusto, illum omnis sapiente accusamus?
           </p>
         </div>
-        <div className="flex flex-col gap-2 button-clr-primary px-4 py-4 rounded-xl justify-center min-h-fit ">
+        <div className="flex flex-col gap-2 button-clr-primary px-4 py-7 rounded-xl justify-center min-h-fit ">
           <span className="w-9 h-9 rounded-full bg-white background_img_card3" />
           <h4 className="text-white font-bold">Intresting Learning</h4>
           <p className="text-white font-thin text-sm">
