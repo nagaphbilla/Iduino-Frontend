f09713f95b0d48c5853430202f7645ff0dbd3a19
vedavyas <vedavyas14042003@gmail.com>
2023-06-03 08:15:19 +0530

    Added videos to the courses

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import Accordition from "../components/Accordition";
 export default function Video() {
+  const [videoSrc,setVideoSrc]=useState('https://www.youtube.com/embed/Nm2Fsi7lLsg')
   return (
     <div>
       <iframe
         className="w-2/3 mx-auto min-height-50 my-5"
-        src="https://www.youtube.com/embed/K5dHGBYcU94"
+        src={videoSrc}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
       <div className="w-2/3 mx-auto">
-        <Accordition />
+        <Accordition setVideoSrc={setVideoSrc}/>
       </div>
     </div>
   );
