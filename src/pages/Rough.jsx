d5118c630c4601f65814fffa016a18f52d2f5653
vedavyas <vedavyas14042003@gmail.com>
2023-06-05 16:41:46 +0530

    Adjusted accordition

--- src/pages/Rough.jsx
+++ src/pages/Rough.jsx
@@ -1,95 +1,60 @@
-import React, { useState, useEffect } from "react";
-import Data from "../Data.json";
+import { Fragment, useState } from "react";
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+} from "@material-tailwind/react";
+import DataJson from "../Data.json";
 
-const Rough = () => {
-  const [data, setData] = useState(
-    Data[0].course1.couseStructure.coursecontent.chapters
+function Icon({ id, open }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={`${
+        id === open ? "rotate-180" : ""
+      } h-5 w-5 transition-transform`}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+    </svg>
   );
-  const toggleAccordion = (index) => {
-    setData((prevData) => {
-      const newData = [...prevData];
-      newData[index].isOpen = !newData[index].isOpen;
-      return newData;
-    });
+}
+
+export default function Example() {
+  const [data, setData] = useState(
+    DataJson[0].course1.couseStructure.coursecontent.chapters
+  );
+  const [open, setOpen] = useState(0);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
   };
 
   return (
-    <div>
+    <Fragment>
       {data.map((item, index) => (
-        <div
-          id={`accordion-${index}`}
-          data-accordion={item.isOpen ? "open" : ""}
-          key={index}
+        <Accordion
+          open={open === index + 1}
+          icon={<Icon id={index + 1} open={open} />}
         >
-          <h2 id={`accordion-heading-${index}`}>
-            <button
-              type="button"
-              className={`flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800`}
-              data-accordion-target={`#accordion-body-${index}`}
-              aria-expanded={item.isOpen ? "true" : "false"}
-              aria-controls={`accordion-body-${index}`}
-              onClick={() => toggleAccordion(index)}
-            >
-              <span className="flex items-center">
-                <svg
-                  className="w-5 h-5 mr-2 shrink-0"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>{" "}
-                {item.title}
-              </span>
-              <svg
-                data-accordion-icon
-                className={`w-6 h-6 ${
-                  item.isOpen ? "rotate-180" : ""
-                } shrink-0`}
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </h2>
-          <div
-            id={`accordion-body-${index}`}
-            className={`hidden ${item.isOpen ? "" : "hidden"}`}
-            aria-labelledby={`accordion-heading-${index}`}
+          <AccordionHeader
+            onClick={() => handleOpen(index + 1)}
+            className="`flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800`"
           >
-            <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-              {item.topics.map((topic, topicIndex) => (
-                <div
-                  className="flex items-center gap-3 mb-2 text-gray-500 dark:text-gray-400 cursor-pointer"
-                  key={topicIndex}
-                  onClick={() => {
-                    // handle topic click
-                  }}
-                >
-                  <input
-                    type="checkbox"
-                    name={`topic-${index}-${topicIndex}`}
-                    id={`topic-${index}-${topicIndex}`}
-                  />
-                  {topic.title}
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+            {item.title}
+          </AccordionHeader>
+          <AccordionBody className="`flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800`">
+            {item.topics.map((topic, index) => (
+              <div className="flex items-center gap-3 mb-2 text-gray-500 dark:text-gray-400 cursor-pointer">
+                {topic.title}
+              </div>
+            ))}
+          </AccordionBody>
+        </Accordion>
       ))}
-    </div>
+    </Fragment>
   );
-};
-
-export default Rough;
+}
